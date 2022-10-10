21a1fe037d97d50361231869a6a8f2dc3bd5f834
Semen Bushmanov <bush.sem@mail.ru>
2022-10-10 13:51:22 +0500

    add banner component

--- src/pages/catalog/catalog.tsx
+++ src/pages/catalog/catalog.tsx
@@ -1,17 +1,13 @@
 import Header from '../../components/header/header';
 import Footer from '../../components/footer/footer';
+import Banner from '../../components/banner/banner';
 
 function Catalog(): JSX.Element {
   return (
     <div className="wrapper">
       <Header />
       <main>
-        <div className="banner">
-          <picture>
-            <source type="image/webp" srcSet="img/content/banner-bg.webp, img/content/banner-bg@2x.webp 2x"/><img src="img/content/banner-bg.jpg" srcSet="img/content/banner-bg@2x.jpg 2x" width="1280" height="280" alt="баннер"/>
-          </picture>
-          <p className="banner__info"><span className="banner__message">Новинка!</span><span className="title title--h1">Cannonball&nbsp;Pro&nbsp;MX&nbsp;8i</span><span className="banner__text">Профессиональная камера от&nbsp;известного производителя</span><a className="btn" href="#">Подробнее</a></p>
-        </div>
+        <Banner />
         <div className="page-content">
           <div className="breadcrumbs">
             <div className="container">
