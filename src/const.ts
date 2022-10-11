ea184e72f41c5fb76dfd28bfc33a333b1f6b8afe
Semen Bushmanov <bush.sem@mail.ru>
2022-10-11 19:52:19 +0500

    add api routes

--- src/const.ts
+++ src/const.ts
@@ -3,3 +3,11 @@ export enum AppRoute {
   Item = '/item',
   Basket = '/basket',
 }
+
+export enum APIRoute {
+  Cameras = '/cameras',
+  Promo = '/promo',
+  Reviews = '/reviews',
+  Coupons = '/coupons',
+  Orders = '/orders',
+}
