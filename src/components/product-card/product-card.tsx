c89f0d194a8e9b9eeb8b68c71dc96b521c65aca4
Semen Bushmanov <bush.sem@mail.ru>
2022-10-11 23:44:51 +0500

    add props to product card component, add images to public folder

--- src/components/product-card/product-card.tsx
+++ src/components/product-card/product-card.tsx
@@ -1,41 +1,45 @@
 import { Link } from 'react-router-dom';
 import { AppRoute } from '../../const';
+import { Camera } from '../../types/camera';
 
 type ProductCardProps = {
+  camera: Camera;
   isInBasket: boolean;
 };
 
-function ProductCard({isInBasket}: ProductCardProps): JSX.Element {
+function ProductCard({camera, isInBasket}: ProductCardProps): JSX.Element {
+  const { name, rating, price, previewImg, previewImg2x, previewImgWebp, previewImgWebp2x, reviewCount } = camera;
+
   return (
     <div className="product-card">
       <div className="product-card__img">
         <picture>
-          <source type="image/webp" srcSet="img/content/img1.webp, img/content/img1@2x.webp 2x"/>
-          <img src="img/content/img1.jpg" srcSet="img/content/img1@2x.jpg 2x" width="280" height="240" alt="Ретрокамера «Das Auge IV»"/>
+          <source type="image/webp" srcSet={`${previewImgWebp}, ${previewImgWebp2x} 2x`}/>
+          <img src={previewImg} srcSet={`${previewImg2x} 2x`} width="280" height="240" alt={name}/>
         </picture>
       </div>
       <div className="product-card__info">
         <div className="rate product-card__rate">
           <svg width="17" height="16" aria-hidden="true">
-            <use xlinkHref="#icon-full-star"></use>
+            <use xlinkHref={rating > 0 ? '#icon-full-star' : '#icon-star'}></use>
           </svg>
           <svg width="17" height="16" aria-hidden="true">
-            <use xlinkHref="#icon-full-star"></use>
+            <use xlinkHref={rating > 1 ? '#icon-full-star' : '#icon-star'}></use>
           </svg>
           <svg width="17" height="16" aria-hidden="true">
-            <use xlinkHref="#icon-full-star"></use>
+            <use xlinkHref={rating > 2 ? '#icon-full-star' : '#icon-star'}></use>
           </svg>
           <svg width="17" height="16" aria-hidden="true">
-            <use xlinkHref="#icon-star"></use>
+            <use xlinkHref={rating > 3 ? '#icon-full-star' : '#icon-star'}></use>
           </svg>
           <svg width="17" height="16" aria-hidden="true">
-            <use xlinkHref="#icon-star"></use>
+            <use xlinkHref={rating > 4 ? '#icon-full-star' : '#icon-star'}></use>
           </svg>
-          <p className="visually-hidden">Рейтинг: 3</p>
-          <p className="rate__count"><span className="visually-hidden">Всего оценок:</span>23</p>
+          <p className="visually-hidden">Рейтинг: {rating}</p>
+          <p className="rate__count"><span className="visually-hidden">Всего оценок:</span>{reviewCount}</p>
         </div>
-        <p className="product-card__title">Ретрокамера «Das Auge IV»</p>
-        <p className="product-card__price"><span className="visually-hidden">Цена:</span>73 450 ₽
+        <p className="product-card__title">{name}</p>
+        <p className="product-card__price"><span className="visually-hidden">Цена:</span>{price} ₽
         </p>
       </div>
       <div className="product-card__buttons">
--- src/components/product-cards-list/product-cards-list.tsx
+++ src/components/product-cards-list/product-cards-list.tsx
@@ -1,9 +1,14 @@
 import ProductCard from '../product-card/product-card';
+import { useAppSelector } from '../../hooks';
+import { getCameras } from '../../store/cameras-data/selectors';
 
 function ProductCardsList(): JSX.Element {
+  const cameras = useAppSelector(getCameras);
+  const camerasToRender = cameras.slice(0, 9);
+
   return (
     <div className="cards catalog__cards">
-      {[...Array(9).keys()].map((item) => <ProductCard key={item} isInBasket={false}/>)}
+      {camerasToRender.map((camera) => <ProductCard key={camera.id} camera= {camera} isInBasket={false}/>)}
     </div>
   );
 }
