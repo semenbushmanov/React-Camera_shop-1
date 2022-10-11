a2b8e9de635083e91645847fbce6b9795288dd0c
Semen Bushmanov <bush.sem@mail.ru>
2022-10-11 21:12:46 +0500

    install http-status-codes and react-toastify, add api services

---
+++ src/services/api.ts
@@ -0,0 +1,33 @@
+import axios, { AxiosInstance, AxiosResponse, AxiosError } from 'axios';
+import { StatusCodes } from 'http-status-codes';
+import { toast } from 'react-toastify';
+
+const StatusCodeMapping: Record<number, boolean> = {
+  [StatusCodes.BAD_REQUEST]: true,
+  [StatusCodes.NOT_FOUND]: true,
+};
+
+const shouldDisplayError = (response: AxiosResponse) => !!StatusCodeMapping[response.status];
+
+const BACKEND_URL = 'https://camera-shop.accelerator.pages.academy/';
+const REQUEST_TIMEOUT = 5000;
+
+export const createAPI = (): AxiosInstance => {
+  const api = axios.create({
+    baseURL: BACKEND_URL,
+    timeout: REQUEST_TIMEOUT,
+  });
+
+  api.interceptors.response.use(
+    (response) => response,
+    (error: AxiosError<{error: string}>) => {
+      if (error.response && shouldDisplayError(error.response)) {
+        toast.warn(error.response.data.error);
+      }
+
+      throw error;
+    }
+  );
+
+  return api;
+};
