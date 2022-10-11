ad5fa86b41725c58adcdef6fd45101e39d3e69d1
Semen Bushmanov <bush.sem@mail.ru>
2022-10-11 19:47:41 +0500

    add camera data types

---
+++ src/types/camera.ts
@@ -0,0 +1,54 @@
+export type Camera = {
+  id: number;
+  name: string;
+  vendorCode: string;
+  type: string;
+  category: string;
+  description: string;
+  level: string;
+  rating: number;
+  price: number;
+  previewImg: string;
+  previewImg2x: string;
+  previewImgWebp: string;
+  previewImgWebp2x: string;
+  reviewCount: number;
+};
+
+export type Review = {
+  id: string;
+  userName: string;
+  advantage: string;
+  disadvantage: string;
+  review: string;
+  rating: number;
+  createAt: string;
+  cameraId: number;
+};
+
+export type Promo = {
+  id: number;
+  name: string;
+  previewImg: string;
+  previewImg2x: string;
+  previewImgWebp: string;
+  previewImgWebp2x: string;
+}
+
+export type ReviewPost = {
+  cameraId: number;
+  userName: string;
+  advantage: string;
+  disadvantage: string;
+  review: string;
+  rating: number;
+};
+
+export type CouponPost = {
+  coupon: 'camera-333' | 'camera-444' | 'camera-555';
+};
+
+export type OrderPost = {
+  camerasIds: number[];
+  coupon: 'camera-333' | 'camera-444' | 'camera-555' | null;
+};
