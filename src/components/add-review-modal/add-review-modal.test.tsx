8d336d225cbb764cb15fa622525bd31c58496d24
Semen Bushmanov <bush.sem@mail.ru>
2022-11-05 23:02:45 +0500

    add test for modal to post reviews

---
+++ src/components/add-review-modal/add-review-modal.test.tsx
@@ -0,0 +1,50 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { Provider } from 'react-redux';
+import { configureMockStore } from '@jedmao/redux-mock-store';
+import AddReviewModal from './add-review-modal';
+import { Promo } from '../../types/camera';
+
+const mockStore = configureMockStore();
+
+const store = mockStore({
+  DATA: {
+    cameras: [],
+    isDataLoading:false,
+    promo: {} as Promo,
+    isPosting: false,
+    reviewSuccess: false,
+  },
+});
+
+
+describe.only('Component: AddReviewModal', () => {
+  it('should render correctly', () => {
+    render(
+      <Provider store={store}>
+        <AddReviewModal cameraId={2} closeAddReviewModal={jest.fn()}/>
+      </Provider>
+    );
+
+    expect(screen.getByText('Оставить отзыв')).toBeInTheDocument();
+    expect(screen.getByText('Рейтинг')).toBeInTheDocument();
+    expect(screen.getByText('Достоинства')).toBeInTheDocument();
+    expect(screen.getByText('Недостатки')).toBeInTheDocument();
+    expect(screen.getByText('Комментарий')).toBeInTheDocument();
+    expect(screen.getByText('Отправить отзыв')).toBeInTheDocument();
+  });
+
+  it('should close modal when user clicks the corresponding button', async () => {
+    const mockCallback = jest.fn();
+
+    render(
+      <Provider store={store}>
+        <AddReviewModal cameraId={2} closeAddReviewModal={mockCallback}/>
+      </Provider>
+    );
+
+    await userEvent.click(screen.getByRole('button', { name: 'Закрыть попап' }));
+
+    expect(mockCallback).toHaveBeenCalled();
+  });
+});
