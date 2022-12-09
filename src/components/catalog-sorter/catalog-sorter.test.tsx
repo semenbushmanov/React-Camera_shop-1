a9669e6e4ef89ee8f0c2507faf518306f46c3bfc
Semen Bushmanov <bush.sem@mail.ru>
2022-12-09 12:56:13 +0500

    add test for catalog-sorter component

--- src/components/catalog-sorter/catalog-sorter.test.tsx
+++ src/components/catalog-sorter/catalog-sorter.test.tsx
@@ -1,7 +1,8 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import CatalogSorter from './catalog-sorter';
 
-describe('Component: CatalogSorter', () => {
+describe.only('Component: CatalogSorter', () => {
   it('should render correctly', () => {
     render(
       <CatalogSorter
@@ -15,5 +16,39 @@ describe('Component: CatalogSorter', () => {
     expect(screen.getByText('Сортировать:')).toBeInTheDocument();
     expect(screen.getByText('по цене')).toBeInTheDocument();
     expect(screen.getByText('по популярности')).toBeInTheDocument();
+    expect(screen.getByRole('radio', { name: 'по цене' })).toBeChecked();
+    expect(screen.getByRole('radio', { name: 'По возрастанию' })).toBeChecked();
+  });
+
+  it('should call onSortCategoryChange when user choose sorting by price', async () => {
+    const onSortCategoryChange = jest.fn();
+    render(
+      <CatalogSorter
+        onSortCategoryChange={onSortCategoryChange}
+        onSortOrderChange={jest.fn}
+        sortCategory={null}
+        sortOrder={null}
+      />
+    );
+
+    await userEvent.click(screen.getByRole('radio', { name: 'по цене' }));
+
+    expect(onSortCategoryChange).toBeCalled();
+  });
+
+  it('should call onSortOrderChange when user choose sorting by order', async () => {
+    const onSortOrderChange = jest.fn();
+    render(
+      <CatalogSorter
+        onSortCategoryChange={jest.fn()}
+        onSortOrderChange={onSortOrderChange}
+        sortCategory={null}
+        sortOrder={null}
+      />
+    );
+
+    await userEvent.click(screen.getByRole('radio', { name: 'По возрастанию' }));
+
+    expect(onSortOrderChange).toBeCalled();
   });
 });
